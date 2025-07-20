c360b965f1c34c2431f13b4dd1210066904cc517
Kirtan <kirtandevadiga@gmail.com>
2025-07-20 14:58:43 +0530

    added some changes

--- vite.config.ts
+++ vite.config.ts
@@ -24,11 +24,12 @@ export default defineConfig({
     },
   },
   root: path.resolve(import.meta.dirname, "client"),
-  build: {
-   outDir: path.resolve(import.meta.dirname, "dist"),
-
-    emptyOutDir: true,
-  },
+  
+   build: {
+  outDir: path.resolve(import.meta.dirname, "dist", "public"),
+  emptyOutDir: true,
+}
+,
   server: {
     fs: {
       strict: true,
